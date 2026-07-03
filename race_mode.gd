f09e10c6737fe5ed2afd87b03a8190fcb7b1815a
POTD725 <83374156+POTD725@users.noreply.github.com>
2026-07-03 13:19:34 -0400

    feat: add race mode state

--- race_mode.gd
+++ race_mode.gd
@@ -1,1 +1,16 @@
 extends Node
+
+const PLAYER_TEAM := "authority"
+const ENEMY_TEAM := "syndicate"
+
+var root: Node
+var root_id := -1
+var chosen_race := ""
+var chosen_rival := ""
+var picker: Control
+var status_label: Label
+var passive_clock := 0.0
+
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	_build_picker()
